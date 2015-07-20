e0f86f5f0f488c0aeb309cf1188b74b8decd41e6
yangzhaofeng <m18561800618@163.com>
2015-07-20 19:14:12 +0800

    银联在线chinapay后台

---
+++ admin/language/zh-CN/payment/chinapay.php
@@ -0,0 +1,24 @@
+<?php
+// Heading
+$_['heading_title']					 = '网银在线 chinapay';
+
+// Text
+$_['text_payment']					 = '支付';
+$_['text_success']					 = '成功: 您已经修改了付款配置!';
+$_['text_account_warning']			 = '<span class="warning"></span>';
+
+// Entry
+$_['entry_id']					 	 = 'Merchant ID:';
+$_['entry_key']						 = 'Key: <br /><span class="help">Specify a key that will used for hash generation</span>:';
+$_['entry_callback']				 = 'Callback URL:';
+$_['entry_total']                    = 'Total:<br /><span class="help">The checkout total the order must reach before this payment method becomes active.</span>';
+$_['entry_geo_zone']				 = 'Geo Zone:';
+$_['entry_status']					 = 'Status:';
+$_['entry_sort_order']			= 'Sort Order:';
+
+// Error
+$_['error_permission']				 = '警告:你没有权限修改中国银联支付模块！';
+$_['error_id']					 	 = 'Merchant ID 必须填写!';
+$_['error_key']					 	 = 'key 必须填写！';
+
+?>
