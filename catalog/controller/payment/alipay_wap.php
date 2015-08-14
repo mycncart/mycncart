e38594dcfdcd131aef84e671fc37273f6b4a03f5
yangzhaofeng <m18561800618@163.com>
2015-08-14 10:17:39 +0800

    增加支付宝手机网页支付异步通知清除相关cart及checkout的session

--- catalog/controller/payment/alipay_wap.php
+++ catalog/controller/payment/alipay_wap.php
@@ -214,6 +214,22 @@ class ControllerPaymentAlipayWap extends Controller {
 			
 					
 				}
+				
+				//清除sesssion，避免手机网页回复后不清除原有的购物车等信息
+				$this->cart->clear();
+				unset($this->session->data['shipping_method']);
+				unset($this->session->data['shipping_methods']);
+				unset($this->session->data['payment_method']);
+				unset($this->session->data['payment_methods']);
+				unset($this->session->data['guest']);
+				unset($this->session->data['comment']);
+				unset($this->session->data['order_id']);
+				unset($this->session->data['coupon']);
+				unset($this->session->data['reward']);
+				unset($this->session->data['voucher']);
+				unset($this->session->data['vouchers']);
+				unset($this->session->data['totals']);
+				unset($this->session->data['cs_shipfrom']);
 			
 					
 				echo "success";	
