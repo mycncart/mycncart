34a3b97cae7aeba3486458c5f963b5d45310d48d
mycncart <opencartchina@qq.com>
2015-09-06 18:27:28 +0800

    微信扫码支付

---
+++ admin/language/zh-HK/payment/qrcode_wxpay.php
@@ -0,0 +1,51 @@
+<?php
+// Heading
+$_['heading_title']      				= '微信掃碼支付';
+
+// Text 
+$_['text_mchid'] 						= '商戶號';
+$_['text_appid'] 						= '公眾賬號APPID或者應用APPID';
+$_['text_key'] 							= 'API密鑰';
+$_['text_appsecret'] 					= 'AppSecret';
+
+
+$_['text_payment']       				= '支付方式';
+$_['text_wxpay']						= '<img src="view/image/payment/wxpay_coc.png" alt="微信掃碼支付接口" title="微信掃碼支付" style="border: 1px solid #EEEEEE;" />';
+$_['text_success']       				= '成功: 已修改微信掃碼支付！';
+$_['text_edit']          				= '編輯微信掃碼支付';
+
+// Entry
+$_['entry_wxpay_mchid'] 				= '商戶號';
+$_['entry_wxpay_appid'] 				= '公眾賬號APPID或者應用APPID';
+$_['entry_wxpay_key'] 					= 'API密鑰';
+$_['entry_wxpay_appsecret'] 			= 'AppSecret';
+$_['entry_wxpay_status'] 		    	= '狀態';
+$_['entry_wxpay_sort_order']   			= '排序';
+$_['entry_total']					 	= '最低金額';
+
+$_['entry_trade_success_status']       	= '交易成功';
+
+$_['entry_geo_zone']				 	= '區域群組';
+$_['entry_log']		 					= '調試日誌';
+
+// Help
+$_['help_mchid']		    			= '這裏填開戶郵件中的商戶號';
+$_['help_appid']		    			= '這裏填開戶郵件中的（公眾賬號APPID或者應用APPID）';
+$_['help_key']		    				= '這裏請使用商戶平臺登錄賬戶和密碼登錄http://pay.weixin.qq.com 平臺設置的“API密鑰”，為了安全，請設置為32字符串';
+$_['help_appsecret']		    		= '該參數在JSAPI支付（open平臺賬戶不能進行JSAPI支付）的時候需要用來獲取用戶openid，可使用APPID對應的公眾平臺登錄http://mp.weixin.qq.com 的開發者中心獲取AppSecret';
+$_['help_total']					 	= '為使該支付方式生效，訂單所需達到的最低金額';
+$_['help_log']			 				= '如果出現異常，通過日誌記錄相關過程，便於分析處理。';
+$_['help_trade_success']			 	= '當交易成功時，訂單要置為的狀態';
+
+
+
+// Tab
+$_['tab_general']					 	= '常規';
+$_['tab_order_status']       		 	= '訂單狀態';
+
+// Error
+$_['error_permission']   				= '警告: 無權修改微信掃碼支付接口！';
+$_['error_wxpay_mchid']      			= '請填寫商戶號！'; 
+$_['error_wxpay_appid']     			= '請填寫公眾賬號APPID或者應用APPID！'; 
+$_['error_wxpay_key']        			= '請填寫API密鑰！'; 
+$_['error_wxpay_appsecret']    			= '請填寫AppSecret！'; 
