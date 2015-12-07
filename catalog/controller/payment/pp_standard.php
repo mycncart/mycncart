2c687afd6a936783a2e5c163f00610aecb64acff
mycncart <opencartchina@qq.com>
2015-12-07 20:17:31 +0800

    bug fix

--- catalog/controller/payment/pp_standard.php
+++ catalog/controller/payment/pp_standard.php
@@ -76,13 +76,13 @@ class ControllerPaymentPPStandard extends Controller {
 			$data['currency_code'] = $order_info['currency_code'];
 			$data['first_name'] = html_entity_decode($order_info['payment_fullname'], ENT_QUOTES, 'UTF-8');
 			$data['last_name'] = '';
-			$data['address1'] = html_entity_decode($order_info['payment_address_1'], ENT_QUOTES, 'UTF-8');
-			$data['address2'] = html_entity_decode($order_info['payment_address_2'], ENT_QUOTES, 'UTF-8');
+			$data['address1'] = html_entity_decode($order_info['payment_address'], ENT_QUOTES, 'UTF-8');
+			$data['address2'] = '';
 			$data['city'] = html_entity_decode($order_info['payment_city'], ENT_QUOTES, 'UTF-8');
 			$data['zip'] = html_entity_decode($order_info['payment_postcode'], ENT_QUOTES, 'UTF-8');
 			$data['country'] = $order_info['payment_iso_code_2'];
 			$data['email'] = $order_info['email'];
-			$data['invoice'] = $this->session->data['order_id'] . ' - ' . html_entity_decode($order_info['payment_firstname'], ENT_QUOTES, 'UTF-8') . ' ' . html_entity_decode($order_info['payment_lastname'], ENT_QUOTES, 'UTF-8');
+			$data['invoice'] = $this->session->data['order_id'] . ' - ' . html_entity_decode($order_info['payment_fullname'], ENT_QUOTES, 'UTF-8');
 			$data['lc'] = $this->session->data['language'];
 			$data['return'] = $this->url->link('checkout/success');
 			$data['notify_url'] = $this->url->link('payment/pp_standard/callback', '', 'SSL');
