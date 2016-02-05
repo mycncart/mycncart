f4456b74075085f0d3aefc769ba802724bfbd234
mycncart <opencartchina@qq.com>
2016-02-05 11:51:22 +0800

    #3

--- catalog/controller/module/blog_search.php
+++ catalog/controller/module/blog_search.php
@@ -4,9 +4,10 @@ class ControllerModuleBlogSearch extends Controller {
 		$this->load->language('module/blog_search');
 
 		$data['heading_title'] = $this->language->get('heading_title');
+		
+		$this->document->addScript('catalog/view/javascript/blog.js');
 
 		$data['text_blog_search'] = $this->language->get('text_blog_search');
-		$data['filter_blog'] = 'filter_blog';
 		
 		if (isset($this->request->get['filter_blog'])) {
 			$data['filter_blog'] = $this->request->get['filter_blog'];
