84bad92b904ba2b3e07b09238e66ebf1e9d9e00f
mycncart <opencartchina@qq.com>
2016-03-24 18:25:17 +0800

    #31

---
+++ admin/language/en-gb/module/weibo_login.php
@@ -0,0 +1,19 @@
+<?php
+// Heading
+$_['heading_title']        		= 'Log In with WeiBo';
+
+//Text
+$_['text_module']          		= 'Modules';
+$_['text_success']         		= 'Success: You have modified Log In with WeiBo module!';
+$_['text_edit']            		= 'Edit Log In with WeiBo Module';
+$_['text_weibo_open_signup']    = 'In developing process. Please do not enable it.';
+
+//Entry
+$_['entry_appkey']      	    = 'AppKey';
+$_['entry_appsecret']      		= 'AppSecret';
+$_['entry_status']         		= 'Status';
+
+//Error
+$_['error_permission']     		= 'Warning: You do not have permission to modify Log In with WeiBo module!';
+$_['error_appkey']      	    = 'AppKey required!';
+$_['error_appsecret']      		= 'AppSecret required!';
---
+++ admin/language/zh-CN/module/weibo_login.php
@@ -0,0 +1,20 @@
+<?php
+
+// Heading
+$_['heading_title']        			= '微博账号登录';
+
+//Text
+$_['text_module']          			= '模组';
+$_['text_success']         			= '成功: 已修改微博账号登录模组！';
+$_['text_edit']            			= '编辑微博账号登录 模组';
+$_['text_weibo_open_signup']        = '处于开发阶段。请勿启用本模组。';
+
+//Entry
+$_['entry_appkey']      	   		= '客户号(AppKey)';
+$_['entry_appsecret']      			= '安全码(AppSecret)';
+$_['entry_status']         			= '状态';
+
+//Error
+$_['error_permission']     			= '警告: 无权限修改 微博账号登录模组！';
+$_['error_appkey']      	   		= '客户号(AppKey)必填！';
+$_['error_appsecret']      			= '安全码(AppSecret)必填！';
