51b315460b2d767ecfe089deed1ad6d6d2a73de7
yangzhaofeng <support@mycncart.com>
2016-09-12 17:20:57 +0800

    改进绑定登陆提示

--- catalog/language/en-gb/extension/module/qq_login.php
+++ catalog/language/en-gb/extension/module/qq_login.php
@@ -4,7 +4,7 @@ $_['heading_title'] 				= 'Login';
 
 // Text
 $_['text_qq_login']      			= 'Click to Login';
-$_['text_qq_login_warning']    		= 'Have got your QQ info, please bind your qq account with %s account.';
+$_['text_qq_login_warning']    		= 'Have got your QQ info, please bind your qq account with %s account. Then, you will login successfully when you click QQ login button in the later.';
 $_['text_qq_fail']  	    		= 'Authorize Fail';
 
 //Error
--- catalog/language/en-gb/extension/module/weibo_login.php
+++ catalog/language/en-gb/extension/module/weibo_login.php
@@ -4,5 +4,5 @@ $_['heading_title'] 				= 'Login';
 
 // Text
 $_['text_weibo_login']      		= 'Click to Login';
-$_['text_weibo_login_warning']    	= 'Have got your Sina WeiBo info, please bind your weibo account with %s account.';
+$_['text_weibo_login_warning']    	= 'Have got your Sina WeiBo info, please bind your weibo account with %s account. Then, you will login successfully when you click weibo login button in the later.';
 $_['text_weibo_fail']  	    		= 'Authorize Fail';
