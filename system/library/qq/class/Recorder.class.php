9713ebca936576e4b741f860bf635bb0a172e3ea
yangzhaofeng <opencart@qq.com>
2017-08-01 09:57:41 +0800

    升级至2.0.0.0

---
+++ system/library/qq/class/Recorder.class.php
@@ -0,0 +1,78 @@
+<?php
+/* PHP SDK
+ * @version 2.0.0
+ * @author connect@qq.com
+ * @copyright © 2013, Tencent Corporation. All rights reserved.
+ */
+
+require_once(CLASS_PATH."ErrorCase.class.php");
+class Recorder{
+    private static $data;
+    private $inc;
+    private $error;
+
+    public function __construct(){
+        $this->error = new ErrorCase();
+
+        //-------读取配置文件
+        //$incFileContents = file(ROOT."comm/inc.php");
+        //$incFileContents = $incFileContents[1];
+        //$this->inc = json_decode($incFileContents);
+		
+		$arr = array(
+			'appid' => QQ_LOGIN_APPID,
+			'appkey' => QQ_LOGIN_APPKEY,
+			'callback' => QQ_CALLBACK_URI,
+			'scope' => 'get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr',
+			'errorReport' => 1,
+			'storageType' => 'file',
+			'host' => 'localhost',
+			'user' => 'root',
+			'password' => 'root',
+			'database' => 'test',
+		);
+		
+		$one = json_encode($arr);
+
+		$this->inc = json_decode($one);
+		
+		
+        if(empty($this->inc)){
+            $this->error->showError("20001");
+        }
+
+        if(empty($_SESSION['QC_userData'])){
+            self::$data = array();
+        }else{
+            self::$data = $_SESSION['QC_userData'];
+        }
+    }
+
+    public function write($name,$value){
+        self::$data[$name] = $value;
+    }
+
+    public function read($name){
+        if(empty(self::$data[$name])){
+            return null;
+        }else{
+            return self::$data[$name];
+        }
+    }
+
+    public function readInc($name){
+        if(empty($this->inc->$name)){
+            return null;
+        }else{
+            return $this->inc->$name;
+        }
+    }
+
+    public function delete($name){
+        unset(self::$data[$name]);
+    }
+
+    function __destruct(){
+        $_SESSION['QC_userData'] = self::$data;
+    }
+}
