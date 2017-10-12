d901686b7d8b98e85632acc8f61c909cd3223b10
yangzhaofeng <opencart@qq.com>
2017-10-12 10:38:35 +0800

    #135

--- system/library/wxpay/wxpayapi.php
+++ system/library/wxpay/wxpayapi.php
@@ -1,4 +1,23 @@
 <?php
+/**
+ * 
+ * 获取客户的真实IP，避免微信h5网页支付wifi环境下无法支付）
+ * @author 青岛万物一体网络科技有限公司
+ *
+ */
+
+function get_client_ip() {
+	if(getenv('HTTP_CLIENT_IP') && strcasecmp(getenv('HTTP_CLIENT_IP'), 'unknown')) {
+		$ip = getenv('HTTP_CLIENT_IP');
+	} elseif(getenv('HTTP_X_FORWARDED_FOR') && strcasecmp(getenv('HTTP_X_FORWARDED_FOR'), 'unknown')) {
+		$ip = getenv('HTTP_X_FORWARDED_FOR');
+	} elseif(getenv('REMOTE_ADDR') && strcasecmp(getenv('REMOTE_ADDR'), 'unknown')) {
+		$ip = getenv('REMOTE_ADDR');
+	} elseif(isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], 'unknown')) {
+		$ip = $_SERVER['REMOTE_ADDR'];
+	}
+	return preg_match ( '/[\d\.]{7,15}/', $ip, $matches ) ? $matches [0] : '';
+}
 
 /**
  * 
