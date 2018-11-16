30e3f1b463399ff17c6b0a8d649edbc30b3145f6
yangzhaofeng <support@mycncart.com>
2018-11-16 16:42:51 +0800

    修复语言翻译bug

--- admin/language/zh-cn/extension/module/banner.php
+++ admin/language/zh-cn/extension/module/banner.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = '横幅广告';
 
 // Text
-$_['text_extension']   = '扩展功能'
+$_['text_extension']   = '扩展功能';
 $_['text_success']     = '成功: 已修改横幅广告模组！';
 $_['text_edit']        = '编辑横幅广告模组 - (在线教程: <a href="http://www.mycncart.com/blog-116.html" target="_blank">http://www.mycncart.com/blog-116.html</a>)';
 
