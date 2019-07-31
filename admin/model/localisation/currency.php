4553590a8e6ebebb36846af80b39d099e9ea28ba
yangzhaofeng <support@mycncart.com>
2019-07-31 15:24:20 +0800

    language, currency ,slide show update

--- admin/model/localisation/currency.php
+++ admin/model/localisation/currency.php
@@ -112,4 +112,67 @@ class ModelLocalisationCurrency extends Model {
 
 		return $query->row['total'];
 	}
+
+	public function refresh() {
+		if (extension_loaded('curl')) {
+			$data = array();
+
+			if ($force) {
+				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE code != '" . $this->db->escape($this->config->get('config_currency')) . "'");
+			} else {
+				$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "currency WHERE code != '" . $this->db->escape($this->config->get('config_currency')) . "' AND date_modified < '" .  $this->db->escape(date('Y-m-d H:i:s', strtotime('-1 day'))) . "'");
+			}
+
+			foreach ($query->rows as $result) {
+				$data[] = $this->config->get('config_currency') . $result['code'] . '=X';
+			}
+
+			$req_url = 'https://api.exchangerate-api.com/v4/latest/'.$this->config->get('config_currency');
+			$content = file_get_contents($req_url);
+			//echo $response_json;exit;
+
+			/*
+			$curl = curl_init();
+
+			curl_setopt($curl, CURLOPT_URL, 'http://download.finance.yahoo.com/d/quotes.csv?s=' . implode(',', $data) . '&f=sl1&e=.csv');
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($curl, CURLOPT_HEADER, false);
+			curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 30);
+			curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+
+			$content = curl_exec($curl);
+
+			curl_close($curl);
+
+			$lines = explode("\n", trim($content));
+
+			foreach ($lines as $line) {
+				$currency = utf8_substr($line, 4, 3);
+				$value = utf8_substr($line, 11, 6);
+
+				if ((float)$value) {
+					$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '" . (float)$value . "', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($currency) . "'");
+				}
+			}
+			*/
+			$result = json_decode($content, true);
+
+			if ($result['rates']) {
+				/*
+				echo "<pre>";
+				print_r($result['rates']);
+				echo "</pre>";
+				exit;
+				*/
+				foreach ($result['rates'] as $key=>$value) {
+					$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '" . (float)$value . "', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($key) . "'");
+				}
+			}
+
+			$this->db->query("UPDATE " . DB_PREFIX . "currency SET value = '1.00000', date_modified = '" .  $this->db->escape(date('Y-m-d H:i:s')) . "' WHERE code = '" . $this->db->escape($this->config->get('config_currency')) . "'");
+
+			$this->cache->delete('currency');
+		}
+	
+	}
 }
